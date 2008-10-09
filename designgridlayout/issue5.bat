d615f646fceb5d51973ed26d5c6fbb3c467109b8
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2008-10-09 16:07:56 +0000

    Updated batch to add correct command line args to test issue 5

--- designgridlayout/issue5.bat
+++ designgridlayout/issue5.bat
@@ -1,1 +1,1 @@
-java -cp target/designgridlayout-1.0-SNAPSHOT-tests.jar;target/designgridlayout-1.0-SNAPSHOT.jar;libs/swing-layout-1.0.2.jar net.java.dev.designgridlayout.Issue5
+java -cp target/designgridlayout-1.0-SNAPSHOT-tests.jar;target/designgridlayout-1.0-SNAPSHOT.jar;libs/swing-layout-1.0.2.jar net.java.dev.designgridlayout.Issue5 true true
