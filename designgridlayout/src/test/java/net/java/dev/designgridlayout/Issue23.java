24e947234a8551b7cafde2269e8e5559862d16d9
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2008-11-09 05:44:21 +0000

    Release of 1.0 second release candidate

---
+++ designgridlayout/src/test/java/net/java/dev/designgridlayout/Issue23.java
@@ -0,0 +1,36 @@
+//  Copyright 2008 Jason Aaron Osgood, Jean-Francois Poilpret
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package net.java.dev.designgridlayout;
+
+import javax.swing.JSeparator;
+
+public class Issue23 extends AbstractBaseExample
+{
+	public static void main(String[] args)
+	{
+		Issue23 example = new Issue23();
+		example.go(true);
+	}
+
+	@Override public void build(DesignGridLayout layout)
+	{
+		layout.row().left().fill().add(new JSeparator());
+		layout.row().left().fill().add(label("Hello"), new JSeparator());
+		layout.row().center().fill().add(new JSeparator());
+		layout.row().center().fill().add(new JSeparator(), label("Hello"), new JSeparator());
+		layout.row().right().fill().add(new JSeparator());
+		layout.row().right().fill().add(new JSeparator(), label("Hello"));
+	}
+}
