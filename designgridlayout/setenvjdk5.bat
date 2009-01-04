b299afcba0abc437cf56a9aba348a6b74a546469
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2009-01-04 08:21:57 +0000

    Added command scripts (for Windows) to set maven environment to JDK5 or JDK6.

---
+++ designgridlayout/setenvjdk5.bat
@@ -0,0 +1,2 @@
+rem You must make sure that JAVA_HOME_15 is defined and points to JDK 1.5 directory
+set JAVA_HOME=%JAVA_HOME_15%
---
+++ designgridlayout/setenvjdk6.bat
@@ -0,0 +1,2 @@
+rem You must make sure that JAVA_HOME_16 is defined and points to JDK 1.6 directory
+set JAVA_HOME=%JAVA_HOME_16%
