fe65ecc67242c9955777f09d2af8fbd2ad47e252
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2009-02-04 00:33:53 +0000

    Small refactoring (DRY) in sync TC.

--- designgridlayout/src/test/java/net/java/dev/designgridlayout/AbstractSyncLayoutExample.java
+++ designgridlayout/src/test/java/net/java/dev/designgridlayout/AbstractSyncLayoutExample.java
@@ -1,3 +1,17 @@
+//  Copyright 2009 Jason Aaron Osgood, Jean-Francois Poilpret
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package net.java.dev.designgridlayout;
 
 import javax.swing.BoxLayout;
@@ -14,6 +28,11 @@ public abstract class AbstractSyncLayoutExample extends AbstractBaseExample
 		_vertical = vertical;
 	}
 
+	protected AbstractSyncLayoutExample()
+	{
+		this(true);
+	}
+
 	@Override protected void build(DesignGridLayout layout)
 	{
 	}
@@ -26,10 +45,7 @@ public abstract class AbstractSyncLayoutExample extends AbstractBaseExample
     	_frame.setDefaultCloseOperation(exitOnClose
     		? JFrame.EXIT_ON_CLOSE
     		: WindowConstants.DISPOSE_ON_CLOSE);
-    	_top = new JPanel();
-    	BoxLayout box = 
-    		new BoxLayout(_top, _vertical ? BoxLayout.Y_AXIS : BoxLayout.X_AXIS);
-    	_top.setLayout(box);
+    	_top = createTopPanel();
     	_top.setName("TOP");
     	build();
     
@@ -39,8 +55,17 @@ public abstract class AbstractSyncLayoutExample extends AbstractBaseExample
     	_frame.setLocationRelativeTo(null);
     	_frame.setVisible(true);
     }
+	
+	protected JPanel createTopPanel()
+	{
+    	JPanel top = new JPanel();
+    	BoxLayout box = new BoxLayout(
+    		top, _vertical ? BoxLayout.Y_AXIS : BoxLayout.X_AXIS);
+    	top.setLayout(box);
+		return top;
+	}
 
-	protected DesignGridLayout createSubPanel(Border border)
+	protected DesignGridLayout createSubPanel(Border border, String position)
 	{
     	JPanel panel = new JPanel();
     	DesignGridLayout layout = new DesignGridLayout(panel);
@@ -48,13 +73,23 @@ public abstract class AbstractSyncLayoutExample extends AbstractBaseExample
     	{
     		panel.setBorder(border);
     	}
-    	_top.add(panel);
+    	_top.add(panel, position);
     	return layout;
+	}
+	
+	protected DesignGridLayout createSubPanel(Border border)
+	{
+		return createSubPanel(border, null);
+	}
+	
+	protected DesignGridLayout createSubPanel(String position)
+	{
+    	return createSubPanel(null, position);
 	}
 	
 	protected DesignGridLayout createSubPanel()
     {
-		return createSubPanel(null);
+		return createSubPanel(null, null);
     }
 	
 	@Override protected JTextField field(String text)
