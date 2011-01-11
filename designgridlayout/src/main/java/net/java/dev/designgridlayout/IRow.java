8420e352e2db2aa14b42c5e30565a9aed3cfbe88
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2011-01-11 20:44:50 +0000

    Issue #47: added javadoc for new API to show/hide IRow.

--- designgridlayout/src/main/java/net/java/dev/designgridlayout/IRow.java
+++ designgridlayout/src/main/java/net/java/dev/designgridlayout/IRow.java
@@ -54,8 +54,31 @@ public interface IRow
 	 * row)
 	 */
 	public abstract IRow addMulti(JComponent... children);
-	
-	//TODO javadoc
-	public abstract void show();
+
+	/**
+	 * Hides the whole row, ie all its components, forcing its visible height to 0.
+	 * This allows to handle dynamic layouts where some rows appear or disappear based
+	 * on some user settings or actions.
+	 * <p/>
+	 * An {@code IRow} that has been hidden can be shown again by calling {@link #show()}.
+	 * <p/>
+	 * Note that you can call {@code hide()} several times, but you must then call 
+	 * {@link #show()} the same number of times to make the row visible again.
+	 * <p/>
+	 * When a row containing a spanned component (see {@link ISpannableGridRow#spanRow()})
+	 * is hidden, resize behavior is not well handled unless all rows containing the same
+	 * spanning component are also hidden altogether (which is the common use case).
+	 */
 	public abstract void hide();
+	
+	/**
+	 * Restores the whole row to a visible state, ie all its components' visibilities
+	 * are restored to their original state prior to calling {@link #hide()}.
+	 * <p/>
+	 * Note that calling {@code show()} on an already visible row has no effect at all.
+	 * <p/>
+	 * Also, if {@link #hide()} has been called several times, then {@code show()} will
+	 * have to be called the same number of times for the row to become visible again.
+	 */
+	public abstract void show();
 }
