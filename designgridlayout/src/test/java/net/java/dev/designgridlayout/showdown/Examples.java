73e783f3f1bcd3da2efea6f963bef47c0419ea5f
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2011-10-17 20:25:58 +0000

    Added more showdown examples for Componentizer; commented deprecation in MultiComponentExample.

--- designgridlayout/src/test/java/net/java/dev/designgridlayout/showdown/Examples.java
+++ designgridlayout/src/test/java/net/java/dev/designgridlayout/showdown/Examples.java
@@ -40,6 +40,8 @@ import net.java.dev.designgridlayout.Basics8RealWorldExample2;
 import net.java.dev.designgridlayout.Basics9RealWorldExample3;
 import net.java.dev.designgridlayout.Componentizer0OneFixedChild;
 import net.java.dev.designgridlayout.Componentizer4OneVariableOneFixedChildren;
+import net.java.dev.designgridlayout.Componentizer8MinAndMorePolicy;
+import net.java.dev.designgridlayout.Componentizer9ManyChildren;
 import net.java.dev.designgridlayout.IndentRowsExample;
 import net.java.dev.designgridlayout.Misc1CustomizedMargins;
 import net.java.dev.designgridlayout.MultiComponentExample;
@@ -227,11 +229,11 @@ public class Examples extends JFrame
 			new Node("One fixed-width component", Componentizer0OneFixedChild.class)));
 		node.add(new DefaultMutableTreeNode(
 			new Node("One variable width field and one button", Componentizer4OneVariableOneFixedChildren.class)));
-		//TODO
-		// Various examples of componentizer API: 1 fixed label + 1 non fixed field (3 examples)
-		// Example inside DGL
-//		node.add(new DefaultMutableTreeNode(
-//			new Node("One component with fixed width", Componentizer0OneFixedChild.class)));
+		node.add(new DefaultMutableTreeNode(
+			new Node("Same example with different width policy", Componentizer8MinAndMorePolicy.class)));
+		node.add(new DefaultMutableTreeNode(
+			new Node("Many components with various width policies", Componentizer9ManyChildren.class)));
+		//TODO Example inside DGL
 		root.add(node);
 
 		node = new DefaultMutableTreeNode("Miscellaneous");
